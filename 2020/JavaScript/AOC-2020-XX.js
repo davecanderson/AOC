7fe822b5009a9ed83c651cc9c857349d85d51d9d
Dave Anderson <dave.anderson@elections.nsw.gov.au>
2020-12-02 09:12:51 +1100

    Update: Script templates by event

---
+++ 2020/JavaScript/AOC-2020-XX.js
@@ -0,0 +1,64 @@
+(function () {
+    'use strict'
+
+    const site = 'https://adventofcode.com';
+    const year = '2020';
+    const day = 'X';
+    const path = `${year}/day/${day}/input`;
+    const url = `${site}/${path}`;
+
+    if (typeof localStorage === "undefined" || localStorage === null) {
+        var LocalStorage = require('node-localstorage').LocalStorage;
+        localStorage = new LocalStorage('./aoc');
+    }
+
+    const testInput1 = ``;
+    const testInput2 = ``;
+
+    const parseInput = function (input) {
+        return input.trim().split('\n');
+    }
+
+    const solvePart1 = function (data) {
+
+    }
+
+    const solvePart2 = function (data) {
+        
+    }
+
+    const testPart1 = function (data) {
+
+    }
+
+    const testPart2 = function (data) {
+
+    }
+
+    const test = function () {
+        testPart1(testInput1);
+        testPart2(testInput2);
+    }
+
+    const solve = function (data) {
+        console.log('Part 1 Answer: %s', solvePart1(data));
+        console.log('Part 2 Answer: %s', solvePart2(data));
+    }
+
+    const run = function () {
+        if (!localStorage[path]) {
+            fetch(url)
+                .then(res => res.text())
+                .then(txt => {
+                    localStorage.setItem(path, txt);
+                    solve(localStorage[path]);
+                });
+        } else {
+            solve(localStorage[path]);
+        }
+    }
+    
+    test();
+    run();
+
+})();
