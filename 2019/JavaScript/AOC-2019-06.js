361fa8bcbf94f1a234a650206b5e4bc52d26da2f
Dave Anderson <dave.anderson@elections.nsw.gov.au>
2020-12-10 08:43:59 +1100

    Update: Answer for 2019 day 6

--- 2019/JavaScript/AOC-2019-06.js
+++ 2019/JavaScript/AOC-2019-06.js
@@ -1,14 +1,13 @@
 (function() {
-'use strict'
+  'use strict'
 
-const site = 'https://adventofcode.com';
-const year = '2019';
-const day = '6';
-const path = `${year}/day/${day}/input`;
-const url = `${site}/${path}`;
+  const site = 'https://adventofcode.com';
+  const year = '2019';
+  const day = '6';
+  const path = `${year}/day/${day}/input`;
+  const url = `${site}/${path}`;
 
-const testInput1 = `
-COM)B
+  const testInput1 = `COM)B
 B)C
 C)D
 D)E
@@ -18,11 +17,9 @@ G)H
 D)I
 E)J
 J)K
-K)L
-`;
+K)L`;
 
-const testInput2 = `
-COM)B
+  const testInput2 = `COM)B
 B)C
 C)D
 D)E
@@ -34,122 +31,93 @@ E)J
 J)K
 K)L
 K)YOU
-I)SAN
-`;
+I)SAN`;
 
-const parseInput = function(input) {
-  var map = {};
-  input.trim().split('\n').forEach(o => mapOrbit(map, o.split(')')));
-  return map;
-}
-
-const getOrbits = function(body) {
-  if(body.orbits.length) {
-    var orbits = 1;
-    body.orbits.forEach(o => orbits += o.orbitCount);
-    return orbits;
-  } 
-  return 0;
-}
-
-const mapOrbit = function(map, o) {
-  map[o[0]] = map[o[0]] || { body: o[0], orbits: [], get orbit() { return this.orbits[0]; }, get orbitCount() { return getOrbits(this); } };
-  map[o[1]] = map[o[1]] || { body: o[1], orbits: [], get orbit() { return this.orbits[0]; }, get orbitCount() { return getOrbits(this); } };
-  map[o[1]].orbits.push(map[o[0]]);
-}
-
-const getTotalOrbits = function(map) {
-  return Object.values(map).map(m => m.orbitCount).reduce((a,c) => c += a);
-}
-
-const getSharedOrbit = function(o1, o2) {
-  if(o1.orbit === o2.orbit) return o1.orbit;
-  if(!o1.orbits.includes(o2.orbit)) return getSharedOrbit(o1, o2.orbit);
-  if(!o2.orbits.includes(o1.orbit)) return getSharedOrbit(o1.orbit, o2);
-  getSharedOrbit(o1.orbit, o2.orbit);
-}
-
-const getTransfers = function(o1, o2) {
-  var transfers = [], sharedOrbit = getSharedOrbit(o1,o2);
-  while(o1.orbit != sharedOrbit) {
-    transfers.push(o1.orbit.body + ' to ' + o1.orbit.orbit.body);
-    o1 = o1.orbit;
+  const parseInput = function(input) {
+    var map = {};
+    input.trim().split('\n').forEach(o => mapOrbit(map, o.split(')')));
+    return map;
   }
-  while(o2.orbit != sharedOrbit) {
-    transfers.push(o2.orbit.body + ' to ' + o2.orbit.orbit.body);
-    o2 = o2.orbit;
+
+  const getOrbits = function(body) {
+    if(body.orbits.length) {
+      return body.orbits.reduce((a,c) => a += getOrbits(c), 1);
+    } 
+    return 0;
   }
-  console.log(transfers.join(' > '));
-  return transfers;
-}
 
-const getOrbitTransfers = function(map, body1, body2) {
-  return getTransfers(map[body1], map[body2]).length;
-}
-
-const solvePart1 = function(data) {
-  return getTotalOrbits(data);
-}
-
-const solvePart2 = function(data) {   
-  return getTransfers(data, 'YOU', 'SAN'); 
-}
-
-const solve = function() {
-  var data = parseInput(localStorage[path]);  
-  console.log('%s Day %s Part 1 Answer: %s', year, day, solvePart1(data));
-  console.log('%s Day %s Part 2 Answer: %s', year, day, solvePart2(data));
-}
-
-const testPart1 = function() {
-  var map = parseInput(testInput1);
-
-  console.log("Test Part 1");
-
-  console.assert(map.COM.orbitCount == 0);
-  console.assert(map.D.orbitCount == 3);
-  console.assert(map.L.orbitCount == 7);
-
-  console.assert(getTotalOrbits(map) == 42); 
-}
-
-const testPart2 = function() {
-  var map = parseInput(testInput2);
-
-  console.log("Test Part 2");
-
-  let so1 = getSharedOrbit(map.YOU, map.L);
-  let so2 = getSharedOrbit(map.YOU, map.SAN);
-
-  console.assert(so1.body === 'K', `YOU and L shared orbit ${so1.body} is not equal to K`);
-  console.assert(so2.body === 'D', `YOU and SAN shared orbit ${so2.body} is not equal to D`);
-
-  //let ot1 = getOrbitTransfers(map.YOU, map.L);
-  //let ot2 = getOrbitTransfers(map.YOU, map.SAN);
-
-  //console.assert(ot1 === 0, `YOU and L orbit transfers ${ot1} is not equal to 0`);
-  //console.assert(ot2 === 4, `YOU and SAN orbit transfers ${ot2} is not equal to 4`);   
-}
-
-const test = function () {
-  testPart1();
-  testPart2();
-}
-
-const run = function() {
-  if(!localStorage[path]) {
-    fetch(url)
-      .then(res => res.text())
-      .then(txt => {
-          localStorage.setItem(path, txt);
-          solve(localStorage[path]);
-      });
-  } else {
-    solve(localStorage[path]);
+  const getSatellites = function(body) {
+    if(body.satelites.length) {
+      return body.satelites.reduce((a,c) => a.concat(getSatellites(c)), body.satelites);
+    } 
+    return [];
   }
-}
 
-test();
-//run();
+  const mapOrbit = function(map, o) {
+    var body = o[0], sat = o[1];
+    map[body] = map[body] || { body: body, orbits: [], satelites: [] };
+    map[sat] = map[sat] || { body: sat, orbits: [], satelites: [] };
+    map[sat].orbits.push(map[body]);
+    map[body].satelites.push(map[sat]);
+  }
+
+  const solvePart1 = function(data) {
+    return Object.keys(data).reduce((a,c) => a += getOrbits(data[c]), 0);
+  }
+
+  const solvePart2 = function(data) {   
+    var you = data['YOU'], santa = data['SAN'], transfers = 0;
+    while(!getSatellites(you).some(s => s.body === 'SAN')) {
+        you = you.orbits[0];
+        transfers++;
+    }
+    while(you.orbits != santa.orbits) {
+        santa = santa.orbits[0];
+        transfers++;          
+    }
+    return transfers - 2;
+  }
+
+  const solve = function(input) {
+    var data = parseInput(input);  
+    console.log('Part 1 Answer: %s', solvePart1(data));
+    console.log('Part 2 Answer: %s', solvePart2(data));
+  }
+
+  const testPart1 = function(data) {
+      
+    console.assert(getOrbits(data['COM']) == 0);
+    console.assert(getOrbits(data['D']) == 3);
+    console.assert(getOrbits(data['L']) == 7);
+      
+    var a = solvePart1(data);
+    console.assert(a == 42); 
+  }
+
+  const testPart2 = function(data) {
+    var a = solvePart2(data);
+    console.assert(a == 4);   
+  }
+
+  const test = function () {
+    testPart1(parseInput(testInput1));
+    testPart2(parseInput(testInput2));
+  }
+
+  const run = function() {
+    if(!localStorage[path]) {
+      fetch(url)
+        .then(res => res.text())
+        .then(txt => {
+            localStorage.setItem(path, txt);
+            solve(localStorage[path]);
+        });
+    } else {
+      solve(localStorage[path]);
+    }
+  }
+
+  test();
+  run();
 
 })();
