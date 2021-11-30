12fcbf32f332f398b3deff0ee6303afced5e6c2a
Dave Anderson <dave.anderson@elections.nsw.gov.au>
2021-11-30 17:43:33 +1100

    Update: Prepare for AoC 2021 using Node.js
    Solution template for Node.js
    
    Requires NPM to install node-localstorage
    Consider using `fs.readFile` instead of localStorage
    Auth issues prevent use of `fetch` to read/write from URL
    Manual download for data instead for each day

---
+++ 2021/JavaScript/AOC-2021-01.js
@@ -0,0 +1,63 @@
+const year = "2021";
+const day = "01";
+
+var LocalStorage = require("node-localstorage").LocalStorage;
+
+const localStorage = new LocalStorage("./scratch");
+const args = process.argv.slice(2);
+
+const testDataP1 = { input: "", answer: "" };
+const testDataP2 = { input: "", answer: "" };
+
+const parseInput = function (input) {
+  return input
+    .trim()
+    .split("\n")
+    .map((i) => parseInt(i));
+};
+
+const solvePart1 = function (data) {
+  return "";
+};
+
+const solvePart2 = function (data) {
+  return "";
+};
+
+const testPart1 = function (data, answer) {
+  var result = solvePart1(data);
+  console.assert(result === answer, `Part 1 answer ${result} is not ${answer}`);
+};
+
+const testPart2 = function (data, answer) {
+  var result = solvePart2(data);
+  console.assert(result === answer, `Part 2 answer ${result} is not ${answer}`);
+};
+
+const test = function () {  
+  if (args.includes("-p1")) {
+    testPart1(parseInput(testDataP1.input), testDataP1.answer);
+  }
+  if (args.includes("-p2")) {
+    testPart2(parseInput(testDataP2.input), testDataP2.answer);
+  }
+};
+
+const solve = function () {
+  const path = `${year}-${day}-input.txt`;
+  const data = parseInput(localStorage[path]);
+
+  if (args.includes("-p1")) {
+    console.log("Part 1 Answer: %s", solvePart1(data));
+  }
+
+  if (args.includes("-p2")) {
+    console.log("Part 2 Answer: %s", solvePart2(data));
+  }
+};
+
+if (args.includes("-test")) {
+  test();
+} else {
+  solve();
+}
