98b5efaf4d426f32ae24927c080536f2ea798125
Dave Anderson <dave.anderson@elections.nsw.gov.au>
2021-12-03 09:37:22 +1100

    Update: Template for Rust solutions

--- 2021/Rust/src/AOC-2021-XX.rs
+++ 2021/Rust/src/AOC-2021-XX.rs
@@ -5,7 +5,7 @@ use std::{
 };
 
 const YEAR = 2021;
-const DAY = 1;
+const DAY = 0;
     
 fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
     BufReader::new(File::open(filename)?).lines().collect()
@@ -15,15 +15,12 @@ fn parse_input(s: &Vec<String>) -> Vec<i32> {
     return s.iter().map(|x| x.parse::<i32>().unwrap()).collect() 
 }
 
-fn find_depth(data: &Vec<i32>, incr: usize) -> i32 {
-    let mut depth = 0;
-    let start = incr - 1;
-    for(i, _item) in data.iter().enumerate() {
-       if i > start {
-           depth += if data[(i-incr)] < data[i] { 1 } else { 0 };
-       }
-    }
-    return depth;
+fn find_answer_1(data: &Vec<i32>) -> i32 {
+    return 0;
+}
+
+fn find_answer_2(data: &Vec<i32>) -> i32 {
+    return 0;
 }
 
 fn main() {
@@ -34,11 +31,9 @@ fn main() {
     let data = lines_from_file(&filename)
         .expect(format!("Unable  to read {}", &filename).as_str());
 
-    let depth = find_depth(&parse_input(&data), 1);
+    let input = parse_input(data);
 
-    println!("Part 1 Answer: {}", depth);
+    println!("Part 1 Answer: {}", find_answer_1(&input));
 
-    let depth = find_depth(&parse_input(&data), 3);
-
-    println!("Part 2 Answer: {}", depth);
+    println!("Part 2 Answer: {}", find_answer_2(&input));
 }
